378c4b40efe0085eaa819d37cfb8a97f6779a75f
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-07 05:39:42 +0100

    Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('flutter App'),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
@@ -47,9 +47,54 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              color: Colors.red,
-              child: Text('LIST OF TX'),
+            Column(
+              children: transaction.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Color.fromARGB(255, 160, 105, 248),
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          '\$: ${tx.amount}'.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(tx.date.toString(),
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ]),
     );
