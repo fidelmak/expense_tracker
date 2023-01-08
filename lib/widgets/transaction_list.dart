ea80d5c680a5082a7d491c8663591790ff94b6af
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-08 08:09:35 +0100

    modified:   lib/widgets/transaction_list.dart

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,33 +1,59 @@
+import 'package:flutter/material.dart';
 
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
-
-class TransactionList extends StatefulWidget {
-
-
-  @override
-  _TransactionListState createState() => _TransactionListState();
-}
-
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction>_userTransaction =[
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 67.56,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 97.56,
-      date: DateTime.now(),
-    ),
-  ],
-
+class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: _userTransaction.map((tx) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromARGB(255, 160, 105, 248),
+                    width: 2,
+                  ),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '\$: ${tx.amount}'.toString(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(DateFormat('yyyy-MM-dd').format(tx.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        )),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
