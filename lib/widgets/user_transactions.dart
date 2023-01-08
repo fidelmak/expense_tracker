1c82315b8c5ad8ee6ff35b83fe61f818d0cb199d
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-08 08:10:04 +0100

    modified:   lib/main.dart

---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,39 @@
+import 'package:expense_tracker/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+import 'new_transaction.dart';
+
+class UserTransactions extends StatefulWidget {
+  const UserTransactions({super.key});
+
+  @override
+  State<UserTransactions> createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 67.56,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 97.56,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(),
+        TransactionList(),
+      ],
+    );
+  }
+}
