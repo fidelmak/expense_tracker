e6ea1f0c645905895ad213b9ee8938ccf235e946
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-09 23:22:53 +0100

    Changes to be committed:

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -32,7 +32,7 @@ class _UserTransactionsState extends State<UserTransactions> {
     return Column(
       children: <Widget>[
         NewTransaction(),
-        TransactionList(),
+        TransactionList(_userTransaction),
       ],
     );
   }
