6be19c8a5d107cac66197142e2bfd8aa17c0be4a
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-21 10:55:13 +0100

    Changes to be committed:

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -8,12 +9,28 @@ class Chart extends StatelessWidget {
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      return {'day': "T", 'amount': 9.99};
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      var totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      print(DateFormat.E(weekDay));
+      print(totalSum);
+
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionValues);
     return Card(
       elevation: 0,
       margin: EdgeInsets.all(20),
